e68572a50e336c9a38415392078a36e38a8c7cfa
Sunil Sankar <sunil@sunil.cc>
2022-07-08 11:31:16 +0200

    Update install.sh

--- tasks/install.sh
+++ tasks/install.sh
@@ -3,7 +3,8 @@ dnf install epel-release -y
 dnf clean all
 dnf update -y
 dnf install sshpass podman python python3-pip gcc git cmake make gcc vim curl libnsl -y
-
+su - vagrant -c "podman --storage-opt overlay.ignore_chown_errors=true pull docker.io/sunilsankar/docker-almalinux"
+su - vagrant -c "podman --storage-opt overlay.ignore_chown_errors=true pull docker.io/sunilsankar/docker-rocky8"
 pip install --upgrade pip setuptools
 pip install --ignore-installed PyYAML
 pip install -r /vagrant/tasks/files/requirements.txt
